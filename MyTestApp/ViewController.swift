d922aaf46fa04fcadc6386377fdd91abdc516945
Russell Jowell <russ.jowell@gmail.com>
2021-08-27 10:29:48 -0400

    Initial Commit

---
+++ MyTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTestApp
+//
+//  Created by Russell Jowell on 8/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
